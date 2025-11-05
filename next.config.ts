b22208ccd769304fb75243851838976557f1444a
fairnetwork1 <fairprotocol.org@gmail.com>
2025-11-05 11:10:15 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -36,3 +37,4 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
